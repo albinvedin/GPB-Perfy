898d6a3fdfa8c3702ab20a85b4d0642729f69b00
Albin Vedin <ealbved@seliiuvd02625.seli.gic.ericsson.se>
2019-04-17 13:28:56 +0200

    Restructure

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"time"
+	pb "github.com/golang/protobuf/proto"
 )
 
 func SumDurations(durations []time.Duration) (time.Duration) {
@@ -14,4 +15,12 @@ func SumDurations(durations []time.Duration) (time.Duration) {
 
 func FilterWarmups(durations []time.Duration, warmup_iterations int) ([]time.Duration) {
 	return durations[warmup_iterations:]
+}
+
+func Pack(message pb.Message) []byte {
+	bytes, err := pb.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }
--- perfy.go
+++ perfy.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	gm "GPB-Perfy/generated/person"
-	sm "GPB-Perfy/static/person"
-	pb "github.com/golang/protobuf/proto"
-	"GPB-Perfy/measure"
-	"GPB-Perfy/helpers"
 	"GPB-Perfy/args"
 	"GPB-Perfy/log"
+	"GPB-Perfy/test"
 	"strconv"
-	"reflect"
 ) 
-
-type fn func() pb.Message
 
 func main() {
 	times, warmup, fileName := args.Fetch()
@@ -23,88 +16,7 @@ func main() {
 	log.Info("Iterations: " + strconv.Itoa(times))
 	log.Info("Warmup: " + strconv.Itoa(warmup))
 
-	testPGV(times, warmup)
-	testRaw(times, warmup, "Raw")
-	
+	test.Time(times, warmup)
+
 	log.Info("END")
-}
-
-func testEncoding(iterations int, warmup_iterations int) {
-
-}
-
-func testPGV(iterations int, warmup_iterations int) {
-	message := new(gm.Person)
-	message.Id = 1000
-	message.Name = "John Doe"
-	message.Email = "john.doe@example.com"
-	testPGVEncoding(iterations, warmup_iterations, message)
-	testPGVDecoding(iterations, warmup_iterations, message)
-}
-
-func testPGVEncoding(iterations int, warmup_iterations int, message *gm.Person) {
-	rElapsedTimes := measure.MeasureRepeatedEncode(message, iterations)
-	filtered := helpers.FilterWarmups(rElapsedTimes, warmup_iterations)
-	log.Debugf("Encoding (%s) - Duration: %s\n", "PGV", helpers.SumDurations(filtered))
-}
-
-func testPGVDecoding(iterations int, warmup_iterations int, message *gm.Person) {
-	bytes, err := pb.Marshal(message)
-	if err != nil {
-		panic(err)
-	}
-	measure.MeasureRepeatedDecode(bytes, new(gm.Person), warmup_iterations)
-	rElapsedTimes := measure.MeasureRepeatedDecode(bytes, new(gm.Person), iterations)
-	filtered := helpers.FilterWarmups(rElapsedTimes, warmup_iterations)
-	log.Debugf("Decoding (%s) - Duration: %s\n", "PGV", helpers.SumDurations(filtered))
-}
-
-/*
-func testPGV(iterations int, warmup_iterations int, version string) {
-	p := new(gm.Person)
-	p.Id = 1000
-	p.Name = "John Doe"
-	p.Email = "john.doe@example.com"
-
-	{
-		rElapsedTimes := measure.MeasureRepeatedEncode(p, iterations)
-		filtered := helpers.FilterWarmups(rElapsedTimes, warmup_iterations)
-		log.Debugf("Encoding (%s) - Duration: %s\n", version, helpers.SumDurations(filtered))
-	}
-
-	{
-		bytes, err := pb.Marshal(p)
-		if err != nil {
-			panic(err)
-		}
-		measure.MeasureRepeatedDecode(bytes, new(gm.Person), warmup_iterations)
-		rElapsedTimes := measure.MeasureRepeatedDecode(bytes, new(gm.Person), iterations)
-		filtered := helpers.FilterWarmups(rElapsedTimes, warmup_iterations)
-		log.Debugf("Decoding (%s) - Duration: %s\n", version, helpers.SumDurations(filtered))
-	}
-}
-*/
-
-func testRaw(iterations int, warmup_iterations int, version string) {
-	p := new(sm.Person)
-	p.Id = 1000
-	p.Name = "John Doe"
-	p.Email = "john.doe@example.com"
-
-	{
-		rElapsedTimes := measure.MeasureRepeatedEncode(p, iterations)
-		filtered := helpers.FilterWarmups(rElapsedTimes, warmup_iterations)
-		log.Debugf("Encoding (%s) - Duration: %s\n", version, helpers.SumDurations(filtered))
-	}
-
-	{
-		bytes, err := pb.Marshal(p)
-		if err != nil {
-			panic(err)
-		}
-		measure.MeasureRepeatedDecode(bytes, new(gm.Person), warmup_iterations)
-		rElapsedTimes := measure.MeasureRepeatedDecode(bytes, new(gm.Person), iterations)
-		filtered := helpers.FilterWarmups(rElapsedTimes, warmup_iterations)
-		log.Debugf("Decoding (%s) - Duration: %s\n", version, helpers.SumDurations(filtered))
-	}
 }
