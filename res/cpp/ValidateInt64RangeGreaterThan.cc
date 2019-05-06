b730ef6e936c56b4f2e707cfd50dfbc2e0cc4349
Love Bäckman <ebaclov@seliiuvd02915.seli.gic.ericsson.se>
2019-05-06 14:45:33 +0200

    c++ test

--- res/cpp/ValidateInt64RangeGreaterThan.cc
+++ res/cpp/ValidateInt64RangeGreaterThan.cc
@@ -1,76 +1,64 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <chrono>
+#include <cstdint>
 #include "../pgv/gen/cpp/int64.pb.h"
 #include "../pgv/gen/cpp/int64.pb.validate.h"
 
-pgv::Int64RangeGreaterThan createMessage(int elementCount) {
-  pgv::Int64RangeGreaterThan message {};
-  message.add_content(101);
-  message.add_content(102);
+pgv::Int64RangeGreaterThan createMessage(
+    int messageLength
+){
+  auto message = pgv::Int64RangeGreaterThan();
+  for (int i = 0; i < messageLength; ++i) {
+    message.add_content(105);
+  }
   return message;
 }
 
-int main(int argc, char** argv) {
-  int const iterations { std::stoi(argv[1]) };
-  int const warmup { std::stoi(argv[2]) };
-  int const elementCount { std::stoi(argv[3]) };
+std::int64_t validateOne(
+    pgv::Int64RangeGreaterThan const& message
+  , pgv::ValidationMsg &err
+){
+  auto t1 = std::chrono::high_resolution_clock::now();
+  pgv::Validate(message, &err);
+  auto t2 = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+}
 
-  std::cout << iterations << "\n"
-            << warmup << "\n"
-            << elementCount << std::endl;
-
-  pgv::Int64RangeGreaterThan const message { createMessage(elementCount) };
-  pgv::ValidationMsg err {};
-
-  if (pgv::Validate(message, &err)) {
-    std::cout << "true" << std::endl;
-  } else {
-    std::cout << "false" << std::endl;
+std::vector<std::int64_t> validateN(
+    int const iterations
+  , int const warmup
+  , pgv::Int64RangeGreaterThan const& message
+  , pgv::ValidationMsg &err
+){
+  auto elapsedTimes = std::vector<std::int64_t>();
+  for (int i = 0; i < iterations; ++i) {
+    auto elapsedTime = validateOne(message, err);
+    if (i >= warmup) {
+      elapsedTimes.push_back(elapsedTime);
+    }
   }
+  return elapsedTimes;
+}
+
+int main(
+    int argc
+  , char** argv
+){
+  auto const iterations = std::stoi(argv[1]);
+  auto const warmup = std::stoi(argv[2]);
+  auto const elementCount = std::stoi(argv[3]);
+
+  auto err = pgv::ValidationMsg();
+  auto elapsedTimes = validateN(
+      iterations, warmup, createMessage(elementCount), err);
+
+  std::cout << "[" << elapsedTimes.at(0);
+  for (auto it = elapsedTimes.begin() + 1; it != elapsedTimes.end(); ++it) {
+    std::cout << ", " << *it;
+  }
+  std::cout << "]" << std::endl;
 
   return 0;
 }
-
-/*
-func main() {
-	iterations, warmup, elementCount := helpers.ValidateRangeTest(os.Args)
-
-	elapsedTimes := validateN(iterations, warmup, createMessage(elementCount))
-
-	output, err := json.Marshal(elapsedTimes)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(output))
-}
-
-func validateN(iterations int, warmup int, message *pgv.DoubleRange) []int64 {
-	var elapsedTimes []int64
-	for i := 0; i < iterations; i++ {
-		elapsedTime := validate(message)
-		if i >= warmup {
-			elapsedTimes = append(elapsedTimes, elapsedTime)
-		}
-	}
-	return elapsedTimes
-}
-
-func validate(message *pgv.DoubleRange) int64 {
-	startTime := time.Now()
-	err := message.Validate()
-	elapsedTime := time.Since(startTime)
-	if err != nil {
-		panic(err)
-	}
-	return elapsedTime.Nanoseconds()
-}
-
-func createMessage(messageLength int) *pgv.DoubleRange {
-	message := new(pgv.DoubleRange)
-	for i := 0; i < messageLength; i++ {
-		message.Content = append(message.Content, 0)
-	}
-	return message
-}
-*/
